c65afb5edd9a0c35b872610122f84abda4a33125
Marcelo Lima <100053039+limacmarcelo@users.noreply.github.com>
2023-02-01 20:48:03 -0300

    Versão mensagem personalizada

--- Front-End/main.py
+++ Front-End/main.py
@@ -1,23 +1,37 @@
 import pandas as pd
 import streamlit as st
 
+def armazenar_planilha(lista):
+  df = pd.read_excel(lista)
+  return df
 # -------------------- INTERFACE --------------------
 st.header('Automação para disparo de mensagens no WhatsApp')
 
 st.markdown('''
-        ##### **Siga o modelo abaixo para preencher sua planilha**, caso a planilha upada na ferramenta não esteja no formato correto os envios podem não ser concluídos.
+        ##### Faça upload da planilha contendo apenas uma coluna com os telefones que deseja disparar as mensagens. Siga o modelo abaixo para preencher sua planilha.
 ''')
 
-st.table({
+st.dataframe({
     'Telefone': ['21999990000', '21900009999', '21911112222'],
-    'Mensagem': ['Olá, tudo bem?', 'Olá, tudo bem?', 'Olá, tudo bem?']
 })
+
+mensagem = st.text_input(
+  '**Insira a mensagem que deseja disparar em massa:**',
+
+  )
 
 # upload planilha
 planilha = st.file_uploader(
-    '**Faça aqui o upload da sua planilha** :arrow ',
+    '**Faça aqui o upload da sua planilha:**',
     type=['.xlsx', '.csv']
 )
 
-if planilha:
-    st.success('Planilha recebida!')
+if planilha and mensagem:
+  result = st.button('Enviar', type='primary')
+  on_click=armazenar_planilha(planilha)
+  df = armazenar_planilha(planilha)
+  df['Mensagem teste:'] = mensagem
+else:
+  st.warning('Preencha todos os campos.')
+
+st.dataframe(df)
